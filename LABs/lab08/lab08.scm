19ff023cfe1771f4cc301439107fc835431931f0
xinruizhao19 <24307140096@m.fudan.edu.cn>
2026-04-07 17:35:32 +0800

    lab08 finished

--- LABs/lab08/lab08.scm
+++ LABs/lab08/lab08.scm
@@ -1,8 +1,24 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+  (if (= num1 num2) 0 (if (> num1 num2) 1 -1))
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2)
+  (cond ((= num1 num2) 0)
+        ((> num1 num2) 1)
+        ((< num1 num2) -1)
+  )
+)
 
-(define (repeat f n) 'YOUR-CODE-HERE)
+(define (composed f g) 
+  (lambda (x) (f (g x)))
+)
+
+(define (repeat f n) 
+  (if (> n 1) 
+      (composed f (repeat f (- n 1))) 
+      f
+  )
+)
 
 (define (max a b)
   (if (> a b)
@@ -14,12 +30,35 @@
       b
       a))
 
-(define (gcd a b) 'YOUR-CODE-HERE)
+(define (gcd a b) 
+  (
+    if (= a b) 
+    a 
+    (gcd (min a b) (- (max a b) (min a b)))
+  )
+)
 
 (define (exp b n)
-  (define (helper n so-far) "YOUR CODE HERE")
+  (define (helper n so-far) 
+    (if (= n 0)
+      so-far
+      (helper (- n 1) (* b so-far))
+    )
+  )
   (helper n 1))
 
-(define (swap s) 'YOUR-CODE-HERE)
+(define (swap s)
+  (define (helper s t)
+    (cond ((null? s) t)
+          ((null? (cdr s)) (append t (list (car s))))
+          (else (helper (cdr (cdr s))
+                        (append t (list (car (cdr s)) (car s)))
+                )
+          )
+    )
+  )
+  (helper s '()))
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+  (lambda (inc) (+ num inc))
+)
