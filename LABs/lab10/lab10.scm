c619dcfa1abbc33ab15a498826edd98ec89dc67a
xinruizhao19 <24307140096@m.fudan.edu.cn>
2026-04-28 15:17:12 +0800

    lab10 finished

---
+++ LABs/lab10/lab10.scm
@@ -0,0 +1,27 @@
+(define (if-program condition if-true if-false)
+  (list `if condition if-true if-false)  
+)
+
+(define (square n) (* n n))
+
+(define (pow-expr base exp) 
+  (
+    if (= exp 0) 1 (
+      if (= exp 1) (list `* base 1) (
+          if (even? exp) (list `square (pow-expr base (/ exp 2))) (list `* base (pow-expr base (- exp 1)))
+      )
+    )
+  )
+)
+
+; 将 expr 包装到 lambda 函数中，作用：延迟求值
+(define-macro (repeat n expr)
+  `(repeated-call ,n (lambda () ,expr)))
+
+; Call zero-argument procedure f n times and return the final result.
+(define (repeated-call n f)
+  (if (= n 1)
+      (f)
+      (begin (f) (repeated-call (- n 1) f))
+  )
+)
