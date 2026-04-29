eb32ea485145428a3b9cbb92cc9371a5659eef88
xinruizhao19 <24307140096@m.fudan.edu.cn>
2026-04-29 14:10:08 +0800

    phrase1 finished

--- PJs/scheme/scheme_eval_apply.py
+++ PJs/scheme/scheme_eval_apply.py
@@ -36,6 +36,12 @@ def scheme_eval(expr, env, _=None): # Optional third argument is ignored
     else:
         # BEGIN PROBLEM 3
         "*** YOUR CODE HERE ***"
+        evaluated_first = scheme_eval(first, env)
+        evaluated_rest = map_link(lambda x: scheme_eval(x, env), rest)
+        if scheme_procedurep(evaluated_first):
+            return scheme_apply(evaluated_first, evaluated_rest, env)
+        else:
+            raise SchemeError('{} is not callable'.format(expr))
         # END PROBLEM 3
 
 def scheme_apply(procedure, args, env):
@@ -47,10 +53,18 @@ def scheme_apply(procedure, args, env):
     if isinstance(procedure, BuiltinProcedure):
         # BEGIN PROBLEM 2
         "*** YOUR CODE HERE ***"
+        s = []
+        cur = args
+        while cur is not Link.empty:
+            s.append(cur.first)
+            cur = cur.rest
+        if procedure.need_env is True:
+            s.append(env)
         # END PROBLEM 2
         try:
             # BEGIN PROBLEM 2
             "*** YOUR CODE HERE ***"
+            return procedure.py_func(*s)
             # END PROBLEM 2
         except TypeError as err:
             raise SchemeError('incorrect number of arguments: {0}'.format(procedure))
